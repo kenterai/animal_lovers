16055f8bfdd9e7079a21018c96a54164c64d0efc
EC2 Default User <ec2-user@ip-172-31-41-89.ap-northeast-1.compute.internal>
2022-02-24 13:21:47 +0000

    [Add]登録情報編集画面

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,27 @@
+class Public::UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+       redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :profile, :profile_image)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    resources :categories, only:[:index, :create, :edit, :update]
-  end
-
-  scope module: :public do
-    root to: "homes#top"
-    get 'about' => 'homes#about', as: 'about'
-  end
-
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
 
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about', as: 'about'
+    resources :users, only:[:show, :edit, :update]
+    get 'confirm' => 'users#confirm', as: 'confirm'
+  end
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    resources :categories, only:[:index, :create, :edit, :update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
