118f200e8320b07be0022e59d378b308734f0a3e
EC2 Default User <ec2-user@ip-172-31-41-89.ap-northeast-1.compute.internal>
2022-03-10 12:49:46 +0000

    [Update]投稿一覧画面

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,7 +2,11 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
+    @categories =Category.all
     @posts = Post.all.order('id DESC')
+    if params[:category_id]
+      @category = @categories.find(params[:category_id])
+      @posts = @posts.where(category: @category)
+    end
   end
-
 end
