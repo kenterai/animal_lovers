275335d23155ae227fb46700225770b610fce4bc
EC2 Default User <ec2-user@ip-172-31-41-89.ap-northeast-1.compute.internal>
2022-03-26 07:54:06 +0000

    [Fix] フォロー、フォロワー一覧画面

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -26,13 +26,13 @@ class Public::UsersController < ApplicationController
   end
 
   def follows
-    user = User.find(params[:id])
-    @users = user.followings
+    @user = User.find(params[:id])
+    @users = @user.followings
   end
 
   def followers
-    user = User.find(params[:id])
-    @users = user.followers
+    @user = User.find(params[:id])
+    @users = @user.followers
   end
 
   private
